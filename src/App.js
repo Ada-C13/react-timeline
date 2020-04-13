2a97463664333b23c79c466df187964179f9a6c4
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-04-13 16:17:06 -0700

    Wave 1, crated TimelineEvent component function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
   console.log(timelineData);
@@ -14,9 +15,13 @@ function App() {
         <h1 className="App-title">Application title</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent person = "Adele Goldberg" status = "In Smalltalk, everything happens somewhere else." timestamp = "2018-05-18T22:12:03Z"/>
+        <TimelineEvent person = "Erica Baker" status = "Every once in a while, life affords you the opportunity to have real, authentic, genuine happiness. It's up to you to see it. Pay attention." timestamp = "2018-05-18T22:19:40Z"/>
       </main>
     </div>
   );
 }
+
+
 
 export default App;
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
-}
+const TimelineEvent = (props) => {
+  console.log(props) 
+  return(
+  <div className = "TimelineEvent">
+    <h1>{props.person}</h1>
+    <ul>
+      <li>{props.status}</li>
+      <li>{Timestamp(props.timestamp)}</li>
+    </ul>
+  </div>
+  );
+};
 
 export default TimelineEvent;
