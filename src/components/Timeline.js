843a6193cac7d035878043e1872220d498855e47
Jessica Liang <jessicursliang@gmail.com>
2020-04-13 16:57:05 -0700

    wave 2; format TimelineEvent from data in Timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -3,8 +3,18 @@ import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
 const Timeline = (events) => {
-  
-  return;
+  console.log(events);
+  const eventComponents = events.events.map((event) => {
+    return(
+      <TimelineEvent
+        person = {event.person}
+        status = {event.status}
+        timestamp = {event.timestamp}
+      />
+    );
+  });
+
+  return eventComponents;
 }
 
 export default Timeline;
