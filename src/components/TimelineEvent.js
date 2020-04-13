b5e68c8559e62e0defbc59df8d3552141703973f
Yesenia <yesentorres@gmail.com>
2020-04-13 15:43:25 -0700

    hardcoded TimelineEvent

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
   
-  return;
+  return(
+    <div className ="TimelineEvent">
+      <p>{props.person}</p>
+      <p>{props.status}</p>
+      <p>{props.timestamp}</p>
+    </div>
+  );
 }
 
 export default TimelineEvent;
