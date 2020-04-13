5f690c1c506fe3cc12ea7e1aecd7de58992e1581
Hannah <hannahjoo24@gmail.com>
2020-04-13 14:44:49 -0700

    Updated Wave1

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+const Timeline = (props) => {
+  return (
+    <TimelineEvent 
+      person={props.person} 
+      status={props.status}
+      timestamp={props.timeStamp}
+    />
+  )
 }
 
 export default Timeline;
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
   
-  return;
+  return (
+    <section className="timeline-event">
+      <h1 className="event-person">{ props.person }</h1>
+      <time className="event-time">
+        <Timestamp time={ props.timeStamp }/>
+      </time>
+      <p className="event-status">{ props.status}</p>
+    </section>
+  );
 }
 
 export default TimelineEvent;
