f9ce8c70ed2f2ecc7ae71759bcadb6794b37c793
Rosalyn <syrosalynyu@gmail.com>
2020-04-13 22:40:51 -0700

    use .map to return person's name and message
    1.) use .map to return person's name and message of each event
    2.) apply CSS classes to the "className" attribute of Timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,24 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
+const Timeline = (props) => {
   
-  return;
+  const timelineComponents = props.events.map( (event) => 
+  {
+    return (
+      <TimelineEvent
+        person={event.person}
+        status={event.status}
+        timestamp={event.timestamp}
+      />
+    );
+  });
+
+  return (
+    <p className="Timeline timeline">
+      {timelineComponents}
+    </p>
+  );
 }
 
 export default Timeline;
