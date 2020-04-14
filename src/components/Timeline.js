da2951bda102614dc387fb4b475eaa82755ca3b7
Vera <lizeth.veraro@outlook.com>
2020-04-13 17:49:09 -0700

    Got the props from App(Parent). Arrow funtion, iterate over the data(events) return each event

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,21 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+const Timeline = (props) => {
+  const events = props.eventsData.map((event)=> {
+    return(
+      <TimelineEvent
+        person={event.person}
+        status={event.status}
+        timeStamp={event.timeStamp}
+      />
+    );
+  });
+  return(
+    <p className="timeline">
+     {events}
+    </p>
+  );
 }
 
 export default Timeline;
