54150ae9983319f23ebfdc523020155cdc7a61d5
Olga Patrakova <olgapatrakova@outlook.com>
2020-04-13 17:45:21 -0700

    Wave 1 and part of Wave 2 complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import './components/Timeline.css';
+
+const events = [
+  {
+    person: "Adele Goldberg",
+    status: "In Smalltalk, everything happens somewhere else.",
+    timeStamp: "2018-05-18T22:12:03Z"
+  },
+  {
+    person: "Erica Baker",
+    status: "Every once in a while, life affords you the opportunity to have real, authentic, genuine happiness. It's up to you to see it. Pay attention.",
+    timeStamp: "2018-05-18T22:19:40Z"
+  }
+];
 
 function App() {
   console.log(timelineData);
@@ -11,9 +24,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">Ada Lovelace's social media feed</h1>
       </header>
       <main className="App-main">
+        <Timeline events={events} />
       </main>
     </div>
   );
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
+import './TimelineEvent.css';
 
-const Timeline = () => {
-  
-  return;
-}
+const Timeline = (props) => {
+  const events = props.events.map((event, i) => {
+    return (
+      <div key={i}>
+        <TimelineEvent person={event.person} status={event.status} timestamp={event.timeStamp}/>
+      </div>
+    );
+  });
 
+  return (
+    <ul className="timeline">
+      {events}
+    </ul>
+  );
+};
 export default Timeline;
