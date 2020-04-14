3921f7d5d9826053ef3ab0d076fa3433061bdded
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-04-13 20:52:41 -0700

    Implemented TimelineEvent and Timeline components, with appropriate props passed

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
+
+  console.log(props);
   
-  return;
+  return (
+    <div className="TimelineEvent">
+      <h3>{props.person}</h3>
+      <p>{props.status}</p>
+      <Timestamp time={props.time} />
+    </div>
+  );
 }
 
 export default TimelineEvent;
