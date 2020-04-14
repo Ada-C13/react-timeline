308cb3c0f0193df5a04e891c95269075ecf230f7
Olga Patrakova <olgapatrakova@outlook.com>
2020-04-13 18:49:23 -0700

    Changed the hardcoded Ada Lovelace for {timelineData.person}

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ import './components/Timeline.css';
 const events = timelineData.events
 
 function App() {
-  console.log(timelineData);
 
   // Customize the code below
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Ada Lovelace's social media feed</h1>
+        <h1 className="App-title">{timelineData.person}'s social media feed</h1>
       </header>
       <main className="App-main">
         <Timeline events={events} />
