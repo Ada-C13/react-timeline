164deacd496c20ac34f163d90ae3a90da14a0a61
Corinna Fabre <corinna.fabre@gmail.com>
2020-04-14 08:17:38 -0700

    Wave Two code

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,22 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+const Timeline = (props) => {
+  const TimelineComponents = props.events.map(event => {
+    return (
+      <TimelineEvent key={event.id}
+        person={event.person} 
+        status={event.status}
+        timestamp={event.timeStamp}
+      />
+    );
+  });
+
+  return (
+    <section className="timeline">
+      { TimelineComponents }
+    </section>
+  );
 }
 
 export default Timeline;
