e5b37f786a3d261feb4886289d8fa736eee16ba1
Rosalyn <syrosalynyu@gmail.com>
2020-04-14 10:34:25 -0700

    construct properties in TimelineEvent component
    1.) properties including person, status, and time
    2.) apply CSS classes to associated elements

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
   
-  return;
+  return(
+    <div className="TimelineEvent timeline-event timeline-event:hover">
+      <h3 className="event-person">{props.person}</h3>
+      <Timestamp time={props.timeStamp} className="event-time" />
+      <p className="event-status">{props.status}</p>
+    </div>
+  ) ;
 }
 
 export default TimelineEvent;
