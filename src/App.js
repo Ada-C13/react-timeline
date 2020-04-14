c97e435a5349555e166292c651c3dc53a0e599ad
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-04-14 13:57:05 -0700

    Final cleanup

--- src/App.js
+++ src/App.js
@@ -2,23 +2,16 @@ import React from 'react';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
-import './components/TimelineEvent.css';
-import './components/Timeline.css';
-
-
 
 function App() {
-  console.log(timelineData);
-
   // Customize the code below
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Ada Lovelace's social media feed</h1>
-        {/* <Link href="special.css" rel="stylesheet" type="component/TimelineEvent.css"></LINK> */}
+        <h1 className="App-title">{timelineData.person}'s social media feed</h1> 
       </header>
       <main className="App-main">
-        <Timeline events = {timelineData["events"]}/>
+        <Timeline events = {timelineData["events"]}/> 
       </main>
     </div>
   );
