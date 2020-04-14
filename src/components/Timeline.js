eab05801ff777118776b67ad432d294b99cd093c
Olga Patrakova <olgapatrakova@outlook.com>
2020-04-13 19:18:08 -0700

    Refactor 2

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
-import './TimelineEvent.css';
 
 const Timeline = (props) => {
   const events = props.events.map((event, i) => {
