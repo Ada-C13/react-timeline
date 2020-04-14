66c12e824f286f169453e38d16ff81b98089a9e6
Alicia Combs <alicia.combs.92@gmail.com>
2020-04-14 13:57:53 -0700

    Drafted preliminary version of TimelineEvent

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
   console.log(timelineData);
+
+  const myDict = [{
+    "person": "Adele Goldberg",
+    "status": "In Smalltalk, everything happens somewhere else.",
+    "timeStamp": "2018-05-18T22:12:03Z"
+  },
+  {
+    "person": "Erica Baker",
+    "status": "Every once in a while, life affords you the opportunity to have real, authentic, genuine happiness. It's up to you to see it. Pay attention.",
+    "timeStamp": "2018-05-18T22:19:40Z"
+  }];
 
   // Customize the code below
   return (
@@ -14,6 +26,7 @@ function App() {
         <h1 className="App-title">Application title</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent />
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+
+  return(
+    <div className="TimelineEvent">
+      <h1>{props.name}</h1>
+      <article>{props.message}</article>
+      <footer><Timestamp time={props.timestamp}/></footer>
+    </div>
+  );
 }
 
 export default TimelineEvent;
