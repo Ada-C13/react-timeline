efede2be9209d33ae05dc9d382e417b1e42f24a1
Jessica Stone <jstone4815@gmail.com>
2020-04-14 15:55:41 -0700

    render event-likes in TimelineEvent

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -3,11 +3,15 @@ import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
 const TimelineEvent = (props) => {
+  let likes = "likes";
+  if (props.likes === 1) likes = "like";
+
   return (
     <section className="timeline-event">
       <header className="event-person">{props.person}</header>
       <p className="event-time"><Timestamp time={props.timestamp} /></p>
       <p className="event-status">{props.status}</p>
+      <p className="event-likes">{props.likes} {likes}</p>
     </section>
   )
 }
