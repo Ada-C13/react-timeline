3dd39b2644e00af684f51b89f09bfc39b9ba2f7c
cojenco <icathyouyang@gmail.com>
2020-04-14 14:27:22 -0700

    revise css styling and clean up comments

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -5,25 +5,11 @@ import Timestamp from './Timestamp';
 const TimelineEvent = (props) => {
   
   return(
-    // <div className='timeline-event'>
-    //   <h3 className='event-person'>{props.person}</h3>
-    //   <p className='event-status'>{props.status}</p>
-    //   <Timestamp className='even-time' time={props.oneEvent.timeStamp}/>
-    // </div>
-
     <div className='timeline-event'>
-      <h3 className='event-person'>{props.oneEvent.person}</h3>
-      <p className='event-status'>{props.oneEvent.status}</p>
-      <Timestamp className='even-time' time={props.oneEvent.timeStamp}/>
+      <h3 className='event-person'>{props.person}</h3>
+      <p className='event-status'>{props.status}</p>
+      <section className='event-time'><Timestamp time={props.timestamp}/></section>
     </div>
-
-
-    // <div className='timeline-event'>
-    //   <h3 className='event-person'>Person Name that posted</h3>
-    //   <p className='event-status'>Love that message. CS is like binding math and poetry.</p>
-    //   <p className='even-time'> Timestamp here</p>
-    // </div>
-   
   );
 }
 
