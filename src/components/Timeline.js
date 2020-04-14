ad16a6696ddffde5037c79d83aeeb944b95e033e
mulhoo <oliviamulholland94@gmail.com>
2020-04-14 15:05:27 -0700

    Passes Wave2

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
+const Timeline = (props) => {
+  console.log(props.events)
   
-  return;
+  return (
+    props.events.map((event) => (
+      <TimelineEvent
+         person={event.person}
+         status={event.status}
+         timestamp={event.timestamp}
+      />
+    ))
+  )
 }
 
 export default Timeline;
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  return (
+    <div>
+      <h3>{props.person}</h3>
+      <p>{props.status}</p>
+      <Timestamp time={props.timestamp} />
+    </div>
+  )
 }
 
 export default TimelineEvent;
