3de7f4bf9e8e37e7c5f6e2b30081e3d761cf840c
Jessica Stone <jstone4815@gmail.com>
2020-04-14 13:47:27 -0700

    return formatted data for first person

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  return (
+    <li className="timeline-event">
+      <p className="event-person">{props.data.events[0].person}</p>
+      <Timestamp time={props.data.events[0].timeStamp} />
+      <p className="event-status">{props.data.events[0].status}</p>
+    </li>
+  )
 }
 
 export default TimelineEvent;
