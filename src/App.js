fb2cb25264c011b1a8e890859e598b302c9251f6
SuelyBarreto <suely@barreto.us>
2020-04-13 18:43:14 -0700

    Wave 1 - TimelineEvent

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
   console.log(timelineData);
@@ -14,6 +15,7 @@ function App() {
         <h1 className="App-title">Application title</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent person="Suely Barreto" status="learning" timestamp="04/13/2020 6:29 PM"/>
       </main>
     </div>
   );
