ae72e57349da2ce38c7c2b65f5dceababc1ec51f
Nataliya Pogodina <n.pogodina@gmail.com>
2020-04-14 05:44:01 -0700

    Added sample hardcoded data to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
 
+const feed = [
+  {
+    person: "Adele Goldberg",
+    status: "In Smalltalk, everything happens somewhere else.",
+    timeStamp: "2018-05-18T22:12:03Z"
+  },
+  {
+    person: "Erica Baker",
+    status: "Every once in a while, life affords you the opportunity to have real, authentic, genuine happiness. It's up to you to see it. Pay attention.",
+    timeStamp: "2018-05-18T22:19:40Z"
+  },
+  {
+    person: "Aubrey Tang",
+    status: "The art of computer programming is a blend of mathematics and poetry.",
+    timeStamp: "2018-05-18T22:41:19Z"
+  }
+]
+
 function App() {
   console.log(timelineData);
 
