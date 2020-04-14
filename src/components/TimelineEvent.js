113ce1112b36d8868f5e883d78c3d6443b6c166b
cojenco <icathyouyang@gmail.com>
2020-04-13 17:14:35 -0700

    complete Wave 1 TimelineEvent component

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,29 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
   
-  return;
+  return(
+    // <div className='timeline-event'>
+    //   <h3 className='event-person'>{props.person}</h3>
+    //   <p className='event-status'>{props.status}</p>
+    //   <Timestamp className='even-time' time={props.oneEvent.timeStamp}/>
+    // </div>
+
+    <div className='timeline-event'>
+      <h3 className='event-person'>{props.oneEvent.person}</h3>
+      <p className='event-status'>{props.oneEvent.status}</p>
+      <Timestamp className='even-time' time={props.oneEvent.timeStamp}/>
+    </div>
+
+
+    // <div className='timeline-event'>
+    //   <h3 className='event-person'>Person Name that posted</h3>
+    //   <p className='event-status'>Love that message. CS is like binding math and poetry.</p>
+    //   <p className='even-time'> Timestamp here</p>
+    // </div>
+   
+  );
 }
 
 export default TimelineEvent;
