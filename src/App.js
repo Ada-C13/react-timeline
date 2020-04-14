ea79ed7908944a22f44d94801bd8577fb012fef3
Nikki Vaughan <vnikki13@gmail.com>
2020-04-14 13:52:11 -0700

    Finished wave2 by implementing the component Timeline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
-import TimelineEvent from './components/TimelineEvent';
 
 function App() {
-  console.log(timelineData);
-
-  // Customize the code below
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">{timelineData.person}</h1>
       </header>
       <main className="App-main">
-        <TimelineEvent 
-          person='Nikki'
-          status="cool"
-          timestamp="2018-05-18T22:19:40Z"
-        />
+        <Timeline posts={timelineData.events}/>
       </main>
     </div>
   );
