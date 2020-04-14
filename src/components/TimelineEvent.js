f6cc797e58ca48e0cfd98c27d37e40ef0d724849
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-04-14 10:58:37 -0700

    Converted the timestamp in TimelineEvent.js

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './TimelineEvent.css';
-
+import Timestamp from './Timestamp.js';
 const TimelineEvent = (props) => {
   return(
     <div className = "timeline-event">
       <div className = "event-person"><strong>{props.person}</strong></div>
       <div className = "event-status">{props.status}</div>
-      <div className = "event-time">{props.timeStamp}</div> 
+      <div className = "event-time"><Timestamp time = {props.timeStamp}/></div> 
     </div>
   );
 }
