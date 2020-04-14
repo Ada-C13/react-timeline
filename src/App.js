f16120a5fb01207eddf2ca888d71df8d7f700eb1
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-04-14 00:18:09 -0700

    Completed Wave1 with hardcoded

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
   console.log(timelineData);
@@ -11,9 +12,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">Ada Lovelace's social media feed</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent  person = "Adele Goldberg" status = "In Smalltalk, everything happens somewhere else."  timestamp = "2018-05-18T22:12:03Z"/>
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  return(
+    <div className = "TimeLineEvent">
+      
+        <p>{props.person}</p>
+        <p>{props.status}</p>
+        <p>{props.timestamp}</p>
+      
+    </div>
+  );
 }
 
 export default TimelineEvent;
