ae81af55bbc74c9433ef88738762932b79304bb1
Alex Robertson <arobertson335@gmail.com>
2020-04-14 15:57:45 -0700

    Finished Wave One and Wave Two.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
   console.log(timelineData);
-
-  // Customize the code below
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">{timelineData.person}'s Social Media Feed</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent
+        person="Charles Babbage"
+        status="Chaps, no one made 'em like Byron made 'em. #LoveladieFanClub"
+        timestamp="2020-10-31T23:58:42Z"
+        />
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  return(
+    <div className="timelineEvent">
+      <li className="event-person">{props.person}</li>
+      <li className="event-clocked"><Timestamp clocked={props.timeStamp} /></li>
+      <li className="event-status">{props.status}</li>
+    </div> 
+  );
 }
 
 export default TimelineEvent;
