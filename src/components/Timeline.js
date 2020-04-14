ac7e0e07e4fae43473975e74e4507220c6691d19
mulhoo <oliviamulholland94@gmail.com>
2020-04-14 15:09:14 -0700

    added key to TimelineEvent, all tests pass no warning

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -4,13 +4,14 @@ import TimelineEvent from './TimelineEvent';
 
 const Timeline = (props) => {
   console.log(props.events)
-  
+
   return (
-    props.events.map((event) => (
-      <TimelineEvent
-         person={event.person}
-         status={event.status}
-         timestamp={event.timestamp}
+    props.events.map((event, i) => (
+      <TimelineEvent 
+        key ={i}
+        person={event.person}
+        status={event.status}
+        timestamp={event.timestamp}
       />
     ))
   )
