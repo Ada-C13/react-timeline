d18295c842a88d139149983db4ba1292f20e28f2
cojenco <icathyouyang@gmail.com>
2020-04-13 17:38:22 -0700

    complete Wave 2 TimeLine Component

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,23 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
+const Timeline = (props) => {
+  const eventComponents = props.events.map((event) => {
+    return (
+      <TimelineEvent 
+        person={event.person}
+        status={event.status}
+        timestamp={event.timeStamp}
+        key={event.timeStamp}
+      />
+    )
+  });
   
-  return;
+  return(
+    <ul className='timeline'>
+      {eventComponents}
+    </ul>
+  );
 }
 
 export default Timeline;
