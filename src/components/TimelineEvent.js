10756b78140e6af425b3719dab3a70ea18f1e4c6
mulhoo <oliviamulholland94@gmail.com>
2020-04-14 15:59:44 -0700

    playing with CSS

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
+import '../App.css';
 
 const TimelineEvent = (props) => {
   return (
     <div>
-      <h3>{props.person}</h3>
-      <p>{props.status}</p>
-      <Timestamp time={props.timestamp} />
+      <h3 className="Timeline-name">{props.person}</h3>
+      <p className="Timeline-status">{props.status}</p>
+      <Timestamp className="Timeline-timestamp" time={props.timestamp} />
     </div>
   )
 }
