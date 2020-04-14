54e82cf02bf5bcedf9ab0f48411b443f25b0ed8f
Nikki Vaughan <vnikki13@gmail.com>
2020-04-14 13:01:25 -0700

    Finished wave1 by implementing the TimelineEvent component

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  return(
+    <div>
+      <p>{props.person}</p>
+      <p>{props.status}</p>
+      <Timestamp timestamp={props.timestamp} />
+    </div>
+  );
 }
 
 export default TimelineEvent;
