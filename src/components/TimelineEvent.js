33c24efd9163a35d790745578abcf28e7b3d3e7e
dnguye2 <dnguyen1093@gmail.com>
2020-04-13 17:42:10 -0700

    skeleton of timeline event implemented

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -4,7 +4,21 @@ import Timestamp from './Timestamp';
 
 const TimelineEvent = () => {
   
-  return;
+  return(
+    <div className="timeline-event">
+      <div className="event-person">
+        <p>Adele Goldberg</p>
+      </div>
+
+      <div className="event-status">
+        <p>In Smalltalk, everything happens somewhere else.</p>
+      </div>
+
+      <div className="event-time">
+        <Timestamp time="2018-05-18T22:12:03Z"/>
+      </div>
+    </div>
+  );
 }
 
 export default TimelineEvent;
