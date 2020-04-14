67695d045382599e586214e6bf2339250384a961
Jessica Stone <jstone4815@gmail.com>
2020-04-14 14:27:55 -0700

    update Timeline to take props argument & render events

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+const Timeline = (props) => {
+  const timelineComponents = props.events.map((event, i) => {
+    return (
+      <li key={i} className="timeline">
+        <TimelineEvent person={ event.person } status={event.status} timestamp={event.timestamp}/>
+      </li>
+    );
+  });
+
+  return (timelineComponents);
 }
 
 export default Timeline;
