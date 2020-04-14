0dfcf451e9e5173b100a1b42a79c9700bee169b9
RebeccaRoach <rebeccaroach12@gmail.com>
2020-04-13 21:10:43 -0700

    App reads in and renders timelineEventList hard-coded variable from App file instead of Timeline.

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,45 +2,11 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-// to test out that we can read from a list within this file:
-const timelineEventList = [
-  {
-    "person": "Adele Goldberg",
-    "status": "In Smalltalk, everything happens somewhere else.",
-    "timeStamp": "2018-05-18T22:12:03Z"
-  },
-  {
-    "person": "Erica Baker",
-    "status": "Every once in a while, life affords you the opportunity to have real, authentic, genuine happiness. It's up to you to see it. Pay attention.",
-    "timeStamp": "2018-05-18T22:19:40Z"
-  },
-  {
-    "person": "Aubrey Tang",
-    "status": "The art of computer programming is a blend of mathematics and poetry.",
-    "timeStamp": "2018-05-18T22:41:19Z"
-  },
-  {
-    "person": "Julia Evans",
-    "status": "no seriously what if we replaced tech books with informative concise 30 page zines though",
-    "timeStamp": "2018-05-18T23:02:44Z"
-  },
-  {
-    "person": "Stephanie Hurlburt",
-    "status": "I don’t think you can do good work if you’re not at least occasionally talking to a person you’re building for.",
-    "timeStamp": "2018-05-18T23:09:38Z"
-  },
-  {
-    "person": "Yan Zhu",
-    "status": "//for a good time, paste this into twitter page console: c=new AudioContext;n=setInterval(\"for(n+=7,i=k,P='▲.\\\n';i-=1/k;P+=P[i%2?(i%2*j-j+n/k^j)&1:2])j=k/i;doc.innerHTML=P;with(c.createOscillator())frequency.value=200*(j+n/k^j),connect(c.destination),start(),stop(n/k)\",k=64)",
-    "timeStamp": "2018-05-18T23:51:01Z"
-  }
-]
-
 
 const Timeline = (props) => {
   
   // use map here to return event components for each element in event list
-  const timelineEventComponents = timelineEventList.map((event) => {
+  const timelineEventComponents = props.events.map((event) => {
     return (
       <TimelineEvent
         key={event.timeStamp}
