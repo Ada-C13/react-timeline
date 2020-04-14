4d4eca0bcb6fb706e57876eafa7e7366cc7499e9
Corinna Fabre <corinna.fabre@gmail.com>
2020-04-14 08:07:17 -0700

    Wave One code

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,24 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+
+// For wave 1 implement the TimelineEvent component function. It should take 3 props.
+
+// person - the person making the post.
+// status - the message being posted.
+// timestamp - the date-time of the event.
+// Test the component by rendering it with hardcoded data inside the App component. TimelineEvent should use the existing Timestamp component to render the time & date of the event.
+
+const TimelineEvent = (props) => {
+  return (
+    <div className="timeline-event">
+      <p className="event-person">{props.person}</p>
+      <p className="event-status">{props.status}</p>
+      <time className="event-time">
+        <Timestamp time={props.Timestamp}/>
+      </time>
+    </div>
+  );
 }
 
 export default TimelineEvent;
