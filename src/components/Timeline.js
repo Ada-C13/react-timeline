2553b5f720396004f0a06d2dc87e1197a4a78dd7
Angela Nguyen <angethuy@gmail.com>
2020-04-14 06:03:04 -0700

    timeline displaying correctly

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,21 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
-}
+const Timeline = (props) => {
+
+  const events = props.events.map((event, i) => {
+    return(
+      <li key={i}>
+        <TimelineEvent person={event.person} status={event.status} time={event.timeStamp} />
+      </li>
+    );
+  });
+
+  return(
+    <ul className="timeline">
+      {events}
+    </ul>
+  );
+};
 
 export default Timeline;
