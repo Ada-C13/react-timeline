b24029918b0724e54f00799687a153da8dcda12c
Antonia <antonianomamiukor@gmail.com>
2020-04-15 16:11:03 -0700

    finished wave 2

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,20 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
-}
+const Timeline = (props) => {
+  const components = props.events.map((event, i) => {
+  return (
+    <div key={i}>
+      <TimelineEvent person={event.person} status={event.status} time={event.timeStamp}/>
+      </div>
+  );
+});
+
+return(
+  <div className="timeline">
+    {components}
+    </div>
+);
+};
 
 export default Timeline;
