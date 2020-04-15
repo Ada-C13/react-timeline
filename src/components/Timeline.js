4eef64129e796c74ec0aeb4e2c198eadeda34e6e
mulhoo <oliviamulholland94@gmail.com>
2020-04-14 17:57:44 -0700

    removed console.log that was left in

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -3,8 +3,6 @@ import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
 const Timeline = (props) => {
-  console.log(props.events)
-
   return (
     props.events.map((event, i) => (
       <TimelineEvent 
