4c5a2e26d9fad8c40549bf86baa31556b25f1a90
Halahaddad1 <h.m.haddad@gmail.com>
2020-04-15 00:19:40 -0700

    initial events posted but not styled yet

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+
+const Timeline = (props) => {
+  const events = props.timelineEvents.map((item,index) => <TimelineEvent key={index} person={item.person} status={item.status} timeStamp={item.timeStamp}/>);
+  return(
+    <div>
+      {events}
+    </div>
+  );
 }
 
 export default Timeline;
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
   
-  return;
+  return(
+    <div>
+      <h1>{props.person} </h1>
+      <p1>{props.status}</p1>
+      <p1><Timestamp time={props.timeStamp} /></p1>
+    </div>
+  )
 }
 
 export default TimelineEvent;
+
+
+
