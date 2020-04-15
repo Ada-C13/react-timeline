fe3d594f15f844d0b3cda8d6e7c99c1000bd8d25
kate myer <kate.d.mangubat@gmail.com>
2020-04-14 23:34:39 -0700

    added props for TimeLine

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,21 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
+const Timeline = (props) => {
   
-  return;
-}
+  const eventComponents = props.events.map((event, i) => {
+    return (
+      <li key={i}>
+        <TimelineEvent person={ event.person } status={ event.status } timeStamp={ event.timeStamp } />
+      </li>
+    );
+  });
+
+  return (
+    <ul className="timeline">
+      {eventComponents}
+    </ul>
+  );
+};
 
 export default Timeline;
