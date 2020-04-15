e9496feba5dadc81ee121bae26b41ebbbf18a826
Stephanie <stephaniejmars@gmail.com>
2020-04-15 08:58:25 -0700

    cleaned up

--- src/App.js
+++ src/App.js
@@ -9,20 +9,13 @@ import Timestamp from './components/Timestamp';
 function App() {
   console.log(timelineData);
 
-  // Customize the code below
-  // Then read in the /src/data/timeline.json file 
-  // into an array of objects in /src/App.js. In the 
-  // render function of the App component, render a TimeLine component 
-  // using the array of objects as the events prop.
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Ada's Feed</h1>
       </header>
       <main className="App-main">
-        <TimelineEvent person="Erica Baker" 
-        status="Every once in a while, life affords you the opportunity to have real, authentic, genuine happiness. It's up to you to see it. Pay attention."
-        timestamp="2018-05-18T22:19:40Z"/>
+        <Timeline events={timelineData.events} />
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -3,42 +3,15 @@ import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 import { render } from '@testing-library/react';
 
-// //This is a Funtional Component 
-// // - simple function
-// // - recieving props and returning a declaration
-// // - always use Functional Components whenever possible 
-// // - absence of "This" keyword 
-// // -- forced to think of solution without having to use State
-// // - mainly responsible for UI 
 
 const TimelineEvent = (props) => {
-  // console.log(props)
-  // return <h1>Hello {props.name}</h1>;
   return(
     <div>
-      <h1>{props.person}</h1>
+      <h3>{props.person}</h3>
       <p><Timestamp time={props.timestamp}/></p>
-      <h3>{props.status}</h3>
+      <p>{props.status}</p>
     </div>
-  );
-
-
-  
+  );  
 }
 
-// // This is a Class Component 
-// // - more feature rich 
-// // - can maintain their own private data - "State"
-// // - can contain complex UI logic 
-// // - provide lifecycle hooks 
-
-
-// Class TimelineEventClass extends Component {
-//   render(){
-//     return <h1>Class Component Texts</h1>
-//   }
-// }
-
 export default TimelineEvent;
-// export default TimelineEventClass;
-// // would also nn to import on APP.js 
