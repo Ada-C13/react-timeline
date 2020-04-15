25bbd6b069b15cb34d1130ba0641e51c100a8fdb
Lola Llanes <mslourdesllanes@gmail.com>
2020-04-14 23:21:49 -0700

    added timeline and timelinedata const

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,24 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
-}
+const Timeline = (props) => {
+  const TimelineData = props.events.map((event, i) => {
+    return (
+      <Timeline
+        key={i}
+        person={event.person}
+        status={event.status}
+        timeStamp={event.timeStamp}
+      />
+    );
+  });
+  return (
+    <main className="timeline">
+      {TimelineData}
+    </main>
+  );
+};
 
-export default Timeline;
+export default TimelineEvent;
+
+
