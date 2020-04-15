bc64cb4de973fbdbeb21bfb20c5251d270258b8e
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-04-14 19:22:23 -0700

    timelineEvent with props: person, status, timestamp

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import './Timeline.css';
-import TimelineEvent from './TimelineEvent';
+import React from "react";
+import "./Timeline.css";
+import TimelineEvent from "./TimelineEvent";
 
-const Timeline = () => {
-  
-  return;
-}
+const Timeline = (props) => {
+  return (
+    <div>
+      {props.events.map((event) => {
+        return (
+          <TimelineEvent
+            person={event.person}
+            status={event.status}
+            timestamp={event.timeStamp}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default Timeline;
+
+/* <TimelineEvent person="Sara" status="Hi there" timestamp="1 day ago" />
+<TimelineEvent person="Ben" status="I'm ben" timestamp="1 day ago" />
+<TimelineEvent person="Sara" status="Hi there" timestamp="1 day ago" /> */
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import './TimelineEvent.css';
-import Timestamp from './Timestamp';
+import React from "react";
+import "./TimelineEvent.css";
+import Timestamp from "./Timestamp";
 
-const TimelineEvent = () => {
-  
-  return;
-}
+const TimelineEvent = (props) => {
+  return (
+    <div className="card">
+      <Timestamp time={props.timestamp} />
+      <h3>{props.person}</h3>
+      <p>{props.status}</p>
+    </div>
+  );
+};
 
 export default TimelineEvent;
