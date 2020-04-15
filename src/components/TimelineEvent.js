7e8f32536140e5eff43c658e78fee4b09f14cfea
Halahaddad1 <h.m.haddad@gmail.com>
2020-04-15 15:44:57 -0700

    added text wrap

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -7,7 +7,7 @@ const TimelineEvent = (props) => {
   return(
     <div className="timeline-event" >
       <h3 className= "event-person">{props.person} </h3>
-      <p1 className= "event-status">{props.status}</p1>
+      <p3 className= "event-status">{props.status}</p3>
       <p1 className= "event-time"><Timestamp time={props.timeStamp} /></p1>
     </div>
   )
