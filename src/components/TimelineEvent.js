f871b7d7d4ed041c5b73f3c7571a113685e29cd3
Yieni <yaynee8d@gmail.com>
2020-04-14 21:03:32 -0700

    wave one

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
+import PropTypes from 'prop-types';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
   
-  return;
-}
+  return (
+    <section className="timelineEvent">
+      <h1 className="person">{ props.person }</h1>
+      <p className="event-status">{ props.status}</p>
+      <time className="timeStamp">
+        <Timestamp time={ props.timeStamp }/>
+      </time>
+    </section>
+  );
+  
+};
+
+TimelineEvent.propTypes = {
+  person: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+};
 
 export default TimelineEvent;
