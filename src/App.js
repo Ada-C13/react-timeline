9916e1cc3c3732780a8631daf55599e5131533db
Jocelyn Wang <wangjoc@gmail.com>
2020-04-15 07:17:04 -0700

    made some css adjustments

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import Timeline from './components/Timeline';
 function App() {
   console.log(timelineData.events);
   return (
-    <div className="App">
+    <section className="App">
       <header className="App-header">
         <h1 className="App-title">{`${timelineData.person}'s Social Media Feed`}</h1>
       </header>
       <main className="App-main">
         <Timeline events={timelineData.events}/>
       </main>
-    </div>
+    </section>
   );
 }
 
