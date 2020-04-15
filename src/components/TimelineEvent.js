ce97dafcd724d98f015854c98527b69c252e5505
Lola Llanes <mslourdesllanes@gmail.com>
2020-04-14 22:39:44 -0700

    added const timelineComponents to return user name and message

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './TimelineEvent.css';
-import Timestamp from './Timestamp';
+// import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
-}
+const TimelineEvent = (props) => {
+  const timelineComponents = props.events.map((event,) => {
+    return (
+      <TimelineEvent
+        person={event.person}
+        status={event.status}
+        timeStamp={event.timeStamp}
+        
+      />
+    );
+  });
+  return(
+    <main className="Timeline timeline">
+    {timelineComponents}
+    </main>
+  );
+};
 
 export default TimelineEvent;
