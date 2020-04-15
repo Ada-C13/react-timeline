ff7627ec0494b53889815ef7e802a7e5edbd5ac8
Jocelyn Wang <wangjoc@gmail.com>
2020-04-15 07:11:17 -0700

    completed basic structure with data to show up

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,26 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+const Timeline = (events) => {
+  console.log(events);
+  const eventComponents = events.events.map((event) => {
+
+    return(
+      <TimelineEvent
+        person={event.person}
+        status={event.status}
+        timeStamp={event.timeStamp}
+        key={event.status}
+      />
+    );
+
+  });
+
+  return (
+    <section className="timeline">
+      {eventComponents}
+    </section>
+  );
 }
 
 export default Timeline;
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
-import Person from './Person';
 
-const TimelineEvent = () => {
-  return (
-    <div>
-      <Person />
-      <Timestamp />
-    </div>
-  )
+const TimelineEvent = (props) => {
+  
+  return(
+    <section className="timeline-event">
+      <oi className="event-person">{props.person}</oi>
+      <oi className="event-status">{props.status}</oi>
+      <oi className="event-time"><Timestamp time={props.timeStamp} /></oi>
+    </section>
+  );
+
 }
 
 export default TimelineEvent;
