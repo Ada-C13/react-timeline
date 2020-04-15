3812720688ce979ccdd1acb5c9fa1921193a97e4
Halahaddad1 <h.m.haddad@gmail.com>
2020-04-15 00:37:57 -0700

    minor fixes

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -6,7 +6,7 @@ const TimelineEvent = (props) => {
   
   return(
     <div className="timeline-event" >
-      <p1 className= "event-person">{props.person} </p1>
+      <h3 className= "event-person">{props.person} </h3>
       <p1 className= "event-status">{props.status}</p1>
       <p1 className= "event-time"><Timestamp time={props.timeStamp} /></p1>
     </div>
