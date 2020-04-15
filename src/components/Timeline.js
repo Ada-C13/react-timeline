ef419764187da16a26e125d1bcb50f84cf8fc175
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-04-14 22:09:28 -0700

    fixedthe timestamp toshow pastfrom andnot currtime

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import './Timeline.css';
-import TimelineEvent from './TimelineEvent';
+import React from "react";
+import "./Timeline.css";
+import TimelineEvent from "./TimelineEvent";
 
-const Timeline = () => {
-  
-  return;
-}
+const Timeline = (props) => {
+  return (
+    <div>
+      {props.events.map((event) => {
+        return (
+          <TimelineEvent
+            className="timeline"
+            person={event.person}
+            status={event.status}
+            timeStamp={event.timeStamp}
+          />
+        );
+      })}
+      ;
+    </div>
+  );
+};
 
 export default Timeline;
