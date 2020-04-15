5a42c562913ad5fe68674ee0d5b43543bc92771c
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-04-14 18:04:26 -0700

    the app is working

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,23 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
+const Timeline = (props) => {
+  const timelineComponents = props.events.map((event, i) => {
+    return (
+      <TimelineEvent
+        person={event.person}
+        timestamp={event.timestamp}
+        status={event.status}
+        key={i}
+      />
+    );
+  });
   
-  return;
+  return(
+    <div className='timeline'>
+      {timelineComponents}
+    </div>
+  ); 
 }
 
 export default Timeline;
